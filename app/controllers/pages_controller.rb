824eeef254144fc6c1edd2458264cce33b90cdf2
Lincoln Marcolongo <mlincoln1205@gmail.com>
2020-11-30 19:50:09 +0100

    Added complementary fields to sign_up form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
+
+
 
   def home
   end
+
+  def index
+      @users = User.all
+
+      # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+      @markers = @users.geocoded.map do |flat|
+        {
+          lat: flat.latitude,
+          lng: flat.longitude
+        }
+      end
+    end
 end
