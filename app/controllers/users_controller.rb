eb1e5b88d7a381249d69d93c6b608eada689a520
Pedro Eduardo <eduardototi@gmail.com>
2020-11-30 14:15:16 -0300

    geocoding users def

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+    @markers = @flats.geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+  end
 end
