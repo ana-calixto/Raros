d9706c86c94d6abd2415c298167195c6e20e60a0
Fernanda Figueiredo <scudellafig@gmail.com>
2020-12-01 13:19:45 -0300

    Created channel/ applied js to Chatroom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,14 +5,18 @@ def create
   @message.chatroom = @chatroom
   @message.user = current_user
   if @message.save
-    redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+    ChatroomChannel.broadcast_to(
+      @chatroom,
+      render_to_string(partial: "message", locals: { message: @message })
+    )
   else 
     render "chatrooms/show" 
   end
+end
 
   private
   
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :chatroom_id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :diseases, through: :history
-  has_many :messages
+  has_many :messages, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
