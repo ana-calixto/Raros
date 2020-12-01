890116153357c9411185144032dc74891992e663
Pedro Eduardo <eduardototi@gmail.com>
2020-12-01 13:25:54 -0300

    login banner ready

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -4,7 +4,8 @@ const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["No Brasil", "13 milhões de pessoas", "são consideradas", "Raras"],
     typeSpeed: 80,
-    loop: true
+    loop: true,
+    showCursor: true
   });
 }
 
