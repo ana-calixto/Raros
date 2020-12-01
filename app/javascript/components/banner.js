1e264e5c34cd89abfc84992003d10d8944c28188
Pedro Eduardo <eduardototi@gmail.com>
2020-12-01 12:25:17 -0300

    login area ready

---
+++ app/javascript/components/banner.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+const loadDynamicBannerText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["No Brasil", "13 milhões de pessoas", "são consideradas", "Raras"],
+    typeSpeed: 80,
+    loop: true
+  });
+}
+
+export { loadDynamicBannerText };
