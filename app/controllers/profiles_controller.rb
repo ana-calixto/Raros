1ee2bdeccdf51a21a0b409eadecf1a0afa1c24e7
Ana Calixto <aninhacalixto@gmail.com>
2020-12-01 16:55:39 -0300

    cloudnary and following model

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,21 +6,13 @@ class ProfilesController < ApplicationController
   end
 
   def follow
-    if current_user.follow(@user.id)
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.js
-      end
-    end
+    current_user.follow(params[:id])
+    redirect_back
   end
 
   def unfollow
-    if current_user.unfollow(@user.id)
-      respond_to do |format|
-        format.html { redirect_to root_path }
-        format.js { render action: :follow }
-      end
-    end
+    current_user.unfollow(params[:id])
+    redirect_back
   end
 
   private
