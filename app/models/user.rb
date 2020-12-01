7aabc737c1958153f16969f4d670b3c9a76a2bd9
Lincoln Marcolongo <mlincoln1205@gmail.com>
2020-12-01 17:44:26 +0100

    Models, controllers and views of topics ready

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_many :messages
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many :posts, through: :topic
 
 end
