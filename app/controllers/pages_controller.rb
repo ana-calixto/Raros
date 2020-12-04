7bfcab2a47a8b1ec8b7d3fb0bc3d4f6f9dbcaf6e
Pedro Eduardo <eduardototi@gmail.com>
2020-12-04 11:47:38 -0300

    first page update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
 
   def index
     @users = User.all
-    #the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    #the `geocoded` scope filters only users with coordinates (latitude & longitude)
     @markers = @users.geocoded.map do |user|
       {
         lat: user.latitude,
