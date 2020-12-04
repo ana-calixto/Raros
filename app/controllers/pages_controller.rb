690ac1a198b66c6700d31019a67a6cbbfe8aad4b
Ana Calixto <aninhacalixto@gmail.com>
2020-12-04 16:41:50 -0300

    final adjustments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index]
+  skip_before_action :authenticate_user!, only: [:home]
   before_action :set_user, only: [:show]
   before_action :set_chatrooms, only: [:index, :search]
 
