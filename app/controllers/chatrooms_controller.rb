090dd70cf4d4979daf676d8e979d976c5a997bf6
Pedro Eduardo <eduardototi@gmail.com>
2020-12-09 14:29:26 -0300

    chat page ready

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,13 +5,13 @@ class ChatroomsController < ApplicationController
   end
 
   def index
-    
+    set_chatrooms
   end
 
   def create
     @user = User.find(params[:id])
-    existing_chat = find_chat 
-    return redirect_to chatroom_path(existing_chat) if existing_chat.present? 
+    existing_chat = find_chat
+    return redirect_to chatroom_path(existing_chat) if existing_chat.present?
     @chatroom = Chatroom.new(user_id: @user.id, author: current_user)
     if @chatroom.save
       redirect_to chatroom_path(@chatroom)
@@ -29,4 +29,10 @@ class ChatroomsController < ApplicationController
   def find_chat
     Chatroom.find_by(user_id: [@user.id, current_user.id], author: [current_user, @user])
   end
+
+  def set_chatrooms
+    @my_chatrooms = Chatroom.all.where(author: current_user)
+    @chatrooms = Chatroom.all.where(user_id: current_user)
+  end
+
 end
